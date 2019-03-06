8f5e33fef4c8a2259383bf1dcc9f96e5a1102b74
Edward Skalibog <skalibog@gmail.com>
2019-03-06 16:04:13 +0200

    add comments

--- ftp.rb
+++ ftp.rb
@@ -1,1 +1,30 @@
+# frozen_string_literal: true
+
 require 'net/ftp'
+require 'json'
+require 'byebug'
+require 'dotenv'
+
+Net::FTP.open(EMV['FTP_URL']) do |ftp|
+  # Login on server
+  ftp.login(ENV['LOGIN'], ENV['PASSWORD'])
+
+  # Change dir
+  ftp.chdir(ENV['PATH'])
+
+  # Return current remote directory
+  puts ftp.pwd
+
+  # File list (ls -las)
+  list = ftp.list
+  puts list
+
+  # Array files into dir or linux ls
+  array = ftp.nlst
+
+  # Download all files from dir
+  array.each { |el| ftp.get(el) }
+
+  # Close connect
+  ftp.close
+end
